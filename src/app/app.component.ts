2f418080227e1d246c184436c58b7a96eb53126f
Jszigeti <j.szigeti1993@gmail.com>
2025-01-22 01:22:41 +0100

    Add RecipeCard component and update styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RecipeCardComponent } from './components/recipe-card/recipe-card.component';
 
 @Component({
   selector: 'app-root',
+  imports: [RecipeCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
